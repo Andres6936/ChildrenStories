e263ed6bf5a3e48231204a4cca50c52059bef28f
Joan Andrés <andres6936@live.com>
2020-08-05 12:22:31 -0500

    Added panel for show information of book

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    render() {
+        return <div className='container-fluid'>
+            <div className='col-6 mx-auto mt-5'>
+                <div className='row'>
+                    <div className='col-6'>
+
+                    </div>
+
+                    <div className='col-6 border'>
+                        <div className='row'>
+                            <form className='row g-3'>
+                                <div className='row'>
+                                    <div className='col-5'>
+                                        <label className='col-form-label'>Name:</label>
+                                    </div>
+                                    <div className='col-7'>
+                                        <input type='text' className='form-control-plaintext'
+                                               readOnly={true} value='The Three Picks'/>
+                                    </div>
+                                </div>
+
+                                <div className='col-12'>
+                                    <label>Number Pag.:</label>
+                                </div>
+
+                                <div className='col-12'>
+                                    <label>Price:</label>
+                                </div>
+
+                                <div className='col-12'>
+                                    <label>Units Sold:</label>
+                                </div>
+                            </form>
+
+                            <form className='col-12'>
+
+                            </form>
+
+                            <form className='col-12'>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    }
 }
-
-export default App;
