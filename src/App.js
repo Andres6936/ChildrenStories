95a5d8a0f06c4d889829b84eb35e44904aad4541
Joan Andrés <andres6936@live.com>
2020-08-05 15:23:17 -0500

    Added section for buttons and options

--- src/App.js
+++ src/App.js
@@ -13,22 +13,38 @@ export default class App extends Component {
 
                     <div className='col-6'>
                         <div className='row'>
-                            <form className='row border'>
+                            <form className='row'>
                                 <LabelInline label='Name:' value='The Three Picks'/>
                                 <LabelInline label='Number Pag:' value='54'/>
                                 <LabelInline label='Price:' value='$ 255.0'/>
                                 <LabelInline label='Units Sold:' value='3'/>
                             </form>
 
-                            <form className='row border'>
+                            <form className='row'>
                                 <LabelInline label='Name:' value='The Martir'/>
                                 <LabelInline label='Number Pag:' value='25'/>
                             </form>
 
-                            <form className='row border'>
+                            <form className='row'>
                                 <LabelInline label='N° Books Sold' value='64'/>
                                 <LabelInline label='Total Revenue' value='$ 123.2'/>
                             </form>
+                        </div>
+
+                        <div className='row mx-auto mr-4 mt-4'>
+                            <div className='btn-group'>
+                                <button className='btn btn-primary'>Prev</button>
+                                <button className='btn btn-danger'>Buy</button>
+                                <button className='btn btn-primary'>Next</button>
+                            </div>
+                        </div>
+
+                        <div className='row mx-auto mr-4 mt-4'>
+                            <div className='btn-group'>
+                                <button className='btn btn-primary'>Option 0</button>
+                                <button className='btn btn-primary'>Option 1</button>
+                                <button className='btn btn-primary'>Option 2</button>
+                            </div>
                         </div>
                     </div>
                 </div>
